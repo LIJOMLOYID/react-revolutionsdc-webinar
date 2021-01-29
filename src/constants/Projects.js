71be1e1ce1c86bed95f8b1b4350eaaebc4a2c01e
LIJOMLOYID <lijomloyid@gmail.com>
2021-01-29 13:08:47 +0530

    Added new projects

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,43 +1,54 @@
 export const projects = {
-    heading: "Projects",
-    quote: "Ahh, Learning alone is boring, implementation is the real fun",
-    projectList: [
-        {
-            name: "Project 1",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 2",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 3",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 4",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 5",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 6",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 7",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 8",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }, {
-            name: "Project 9",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "https://lijomloyid.github.io/monsters-rolodex/"
-        }
-    ]
-}
+  heading: "Projects",
+  quote: "Ahh, Learning alone is boring, implementation is the real fun",
+  projectList: [
+    {
+      name: "Monsters Rolodex",
+      description:
+        "A React app which fetches Monster picture, name and email from a REST API endpoint and delivers it in the UI",
+      link: "https://lijomloyid.github.io/monsters-rolodex/",
+    },
+    {
+      name: "Crown Clothing",
+      description: "A fully fledged e-commerce web application",
+      link: "https://github.com/LIJOMLOYID/crwn-clothing",
+    },
+    {
+      name: "Ristorante Con Fusion",
+      description: "Web application for Fusion Restaurant in Hong Kong",
+      link: "https://github.com/LIJOMLOYID/Ristorante-Con-Fusion",
+    },
+    {
+      name: "Covid Tracker",
+      description:
+        "Java Spring Boot application to track reported data of confirmed Coronavirus infections COVID 19 (2019 nCoV) around the world. Data source used: Novel Coronavirus (COVID-19) Cases, provided by JHU CSSE",
+      link: "https://github.com/LIJOMLOYID/covid-tracker",
+    },
+    {
+      name: "Goodreads for movies",
+      description: "Goodreads for movies",
+      link: "https://github.com/LIJOMLOYID/goodreads-for-movies-API",
+    },
+    {
+      name: "Tesla battery range calculator",
+      description: "Building Tesla’s battery range calculator with React",
+      link: "https://github.com/LIJOMLOYID/tesla-battery-range-calculator",
+    },
+    {
+      name: "Vidly",
+      description: "Web application for over-the-top content platform vidly",
+      link: "https://github.com/LIJOMLOYID/vidly",
+    },
+    {
+      name: "Phonebook",
+      description:
+        "A project using Spring Boot for implementing a RESTful backend, and Angular for creating a JavaScript-based frontend. It fetches and displays a List of JPA entities from a database, and persists new ones through a form",
+      link: "https://github.com/LIJOMLOYID/phonebook",
+    },
+    {
+      name: "Breaking Bad API",
+      description: "Breaking Bad API",
+      link: "https://github.com/LIJOMLOYID/react-breakingbad-api",
+    },
+  ],
+};
