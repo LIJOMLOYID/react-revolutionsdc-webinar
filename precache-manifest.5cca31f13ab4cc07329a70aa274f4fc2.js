c4ccc179995b3f005d6e7c469276bfade6a3a4ab
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-09 14:09:43 +0530

    Updates

---
+++ precache-manifest.5cca31f13ab4cc07329a70aa274f4fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d77041e2539b20c241fffa7c940ee91",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "5956adfefa836082e507",
+    "url": "/resume/static/css/main.6a378717.chunk.css"
+  },
+  {
+    "revision": "6ecede341cc9e6546a4a",
+    "url": "/resume/static/js/2.7d28a4d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.7d28a4d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5956adfefa836082e507",
+    "url": "/resume/static/js/main.ee00c935.chunk.js"
+  },
+  {
+    "revision": "806ec6d82f07e7a30c80",
+    "url": "/resume/static/js/runtime-main.d86b74a1.js"
+  }
+]);
